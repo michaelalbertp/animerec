cc1c0f3a732dd2396f7778f26419c3e122e2c7aa
Michael Albert Purnama <77499853+michaelalbertp@users.noreply.github.com>
2022-01-06 13:11:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 mkdir -p ~/.streamlit/
 echo "\
-[general]\n\
-email = \"albert.kds@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
+[server] \n\
 port = $PORT\n\
+enableCORS = false\n\
+headless = true \n\
+\n\
 " > ~/.streamlit/config.toml
